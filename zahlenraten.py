bec1acd9ec1f0eafe34c278590edae16c69562c1
Tim Kurtz <tim.kurtz@5minds.de>
2022-11-29 14:41:20 +0100

    Basis zahlenraten spiel

---
+++ zahlenraten.py
@@ -0,0 +1,26 @@
+import random
+
+min = 1
+max = 20
+
+play = True
+
+while(play):
+    print("Willkommen bei Zahlenraten!")
+    print(f"Ich denke mir eine Zahl zwischen {str(min)} und {str(max)} aus, und du musst sie erraten!")
+    print("Viel Spaß :^)")
+
+    randInt = random.randint(1, 20)
+    tries = 0
+    while True:
+        guess = int(input(f"Rate eine Zahl zwischen {str(min)} und {str(max)}\n"))
+        tries += 1
+
+        if(guess == randInt):
+            break
+
+    print(f"Glückwunsch, du hast mit {str(tries)} Versuchen gewonnen.")
+    again = input("Noch einmal spielen? (ja = y)\n")
+    play = again == "y"
+
+print("Okay, bis zum nächsten Mal")
